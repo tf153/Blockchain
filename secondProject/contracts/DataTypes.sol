513c2ac6aee34f88c222c324c418403b57fc3973
Rahul Joshi <tj18022826@gmail.com>
2022-01-18 13:42:31 +0530

    Yesterdays

---
+++ secondProject/contracts/DataTypes.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.5.16;
+
+contract DataTypes {
+    uint256 x = 9;
+    bool chk = true;
+    address owner = msg.sender;
+    bytes32 bmsg = "hello";
+    string smsg = "hellow";
+}
